2399291d2357e9c77e9161bcd8d50d6a4a037e51
bogdanq <bshelomanov98@mail.ru>
2021-02-01 22:21:28 +0300

    update: добавил решения с типовых собеседований

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -364,9 +364,3 @@ export {
   Vector,
   reliableMultiply,
 };
-
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
--- yandex/index.js
+++ yandex/index.js
@@ -1,3 +1,4 @@
+// @TODOЫ
 function getPrimes(n) {
   const primes = { 1: false };
   const primesArray = Array.from({ length: n }, (_, k) => k + 1);
@@ -15,6 +16,7 @@ function getPrimes(n) {
 
 // console.log(getPrimes(10)); // 2 3 5 7
 
+// @TODO
 function check(str) {
   const stack = [];
   let isCheck = true;
@@ -46,6 +48,7 @@ function check(str) {
 // console.log(check("{(})[]")); //false
 // console.log(check("{[}]")); // false
 
+// @TODO
 function regions() {
   const model = {};
 
@@ -61,6 +64,16 @@ function regions() {
         {
           id: 32,
           name: "Алейск",
+          chld: [
+            {
+              id: 320,
+              name: "Алейск2",
+            },
+            {
+              id: 890,
+              name: "Барнаул2",
+            },
+          ],
         },
         {
           id: 89,
@@ -89,5 +102,159 @@ function regions() {
   console.log(model.getNameById(0) === "Все регионы"); // true
   console.log(model.getNameById(32) === "Алейск"); // true
   console.log(model.getNameById(89) === "Барнаул"); // true
+  console.log(model.getNameById(890) === "Барнаул2"); // true
 }
-// regions();
+
+// @TODO Нужно написать функцию которая выполняет другие функции последовательно и асинхронно передавая результат
+const sleep = (ms) => new Promise((rs) => setTimeout(rs, ms));
+
+function chain(fnList, input, timeout) {
+  fnList.reduce((acc, fn) => {
+    return acc.then(async (data) => {
+      await sleep(timeout);
+
+      return fn(data);
+    });
+  }, Promise.resolve(input));
+}
+
+// chain(
+//   [
+//     function (prev) {
+//       console.log(prev);
+//       return prev + 1;
+//     },
+//     function (prev) {
+//       console.log(prev);
+//     },
+//   ],
+//   9,
+//   2000
+// );
+
+function getAnagrams(...words) {
+  const anagrams = {};
+
+  words.forEach((word) => {
+    const sorted = word.split("").sort().join("");
+
+    if (!anagrams[sorted]) {
+      anagrams[sorted] = [];
+    }
+
+    anagrams[sorted].push(word);
+  });
+
+  return Object.values(anagrams);
+}
+
+// @TODO getAnagrams("нос", "сон", "снедь", "днесь"); // [[нос, сон], [снедь, днесь]]
+
+function sum(x) {
+  return function result(y) {
+    if (!y) {
+      return x;
+    }
+
+    return sum(x + y);
+  };
+}
+
+function sumWithManyArgs(...x) {
+  function getSum(args) {
+    return args.reduce((acc, item) => acc + item);
+  }
+
+  return function result(...y) {
+    if (!y.length) {
+      return getSum(x);
+    }
+
+    return sumWithManyArgs(getSum([...x, ...y]));
+  };
+}
+
+// @TODO Реализовать функции five, add, one, seven, subtract, two, чтобы работало:
+
+function checkType(arg, value) {
+  return typeof arg === "function" ? arg(value) : value;
+}
+
+function five(n) {
+  return checkType(n, 5);
+}
+
+function one(n) {
+  return checkType(n, 1);
+}
+
+function seven(n) {
+  return checkType(n, 7);
+}
+
+function two(n) {
+  return checkType(n, 2);
+}
+
+function subtract(n) {
+  return (x) => x - n;
+}
+
+function add(n) {
+  return (x) => x + n;
+}
+// console.log(five(add(one()))); // 6
+// console.log(seven(subtract(two()))); // 5
+// console.log(seven(subtract(two(add(one()))))); // 4
+
+const t1 = {
+  value: 4,
+  next: [
+    {
+      value: 3,
+      next: null,
+    },
+    {
+      value: 3,
+      next: [
+        {
+          value: 3,
+          next: [
+            {
+              value: 3,
+              next: null,
+            },
+            {
+              value: 3,
+              next: [
+                {
+                  value: 3,
+                  next: null,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+function getValuesSum(tree) {
+  const stack = [tree];
+  let values = 0;
+
+  while (stack.length) {
+    const sibling = stack.shift();
+
+    values = values + sibling.value;
+
+    if (sibling.next) {
+      stack.unshift(...sibling.next);
+    }
+  }
+
+  return values;
+}
+
+console.log(getValuesSum(t1)); // 22
