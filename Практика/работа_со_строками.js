f1a08f4d7bea2227e0b91a4857d961e9336c274a
Bogdan Shelomanov <bshelomanov@gmail.com>
2025-03-12 21:59:11 +0300

    обновление задачи по поиску длинной подстроки в строке

--- Практика/работа_со_строками.js
+++ Практика/работа_со_строками.js
@@ -1,5 +1,6 @@
 const tasks = {
   rle,
+  lengthOfLongestSubstring,
 };
 
 /**
@@ -12,7 +13,6 @@ const tasks = {
   Задачу можно решить циклом и сохранять текущуй символ или сделать цикл и смотреть i+1 символ
   Сложность O(n)
 */
-
 function rle(str) {
   let result = "";
   let count = 0;
@@ -32,3 +32,39 @@ function rle(str) {
   return result;
 }
 // rle("AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB") === 'A4B3C2XYZD4E3F3A6B28'
+
+/**
+@TODO
+Скользящее динамическое окно
+нужно хранить индекса left и right, а так же максимальную сумму внутри окна
+формула суммы max = right - left + 1 || max
+как только нашли повтор - необходимо удалить уже записанные буквы (можно в цикле от left до leftPrev)
+left - индекс + 1 (что бы окно попало на следующую от нее бувкв) буквы которая повторялась или 0
+leftPrev - индекс буквы которую сохранили и ее же словили в повторе
+*/
+const lengthOfLongestSubstring = function (s) {
+  let map = new Map();
+  let left = 0;
+  let right = 0;
+  let maxLength = 0;
+
+  while (right < s.length) {
+    if (map.has(s[right])) {
+      const prevLeft = map.get(s[right]);
+
+      for (let i = left; i < prevLeft; i++) {
+        map.delete(s[i]);
+      }
+
+      left = prevLeft + 1;
+    }
+
+    maxLength = Math.max(maxLength, right - left + 1);
+    map.set(s[right], right);
+    right++;
+  }
+
+  return maxLength;
+};
+
+// lengthOfLongestSubstring("appart") === 4
